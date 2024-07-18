6b24394f9306685370411bf33bddc618cb8ea402
Felix Biwott <felixkpt@gmail.com>
2024-07-18 12:05:35 +0300

    Autobuilder: edit leads model and related files

--- app/(pages)/dashboard/auto-page-builder/utils/modelGenerator.ts
+++ app/(pages)/dashboard/auto-page-builder/utils/modelGenerator.ts
@@ -94,6 +94,7 @@ async function generateModel(data: any, fields: any, modelNameSingular: string, 
     // Add and commit changes
     await git.add('.');
     const commitMessage = `Autobuilder: ${actionType} ${data.modelNameSingular.toLowerCase()} model and related files`;
+    console.log('should commit:', commitMessage)
     await git.commit(commitMessage);
 
   } catch (error: any) {

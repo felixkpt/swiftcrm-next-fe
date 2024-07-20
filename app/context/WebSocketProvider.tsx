29d0cd9f08d1824d51120851dd8b01142823e5fd
Felix Biwott <felixkpt@gmail.com>
2024-07-21 00:49:35 +0300

    Autobuilder: edit categories model and related files

---
+++ app/context/WebSocketProvider.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+export const WebSocketContext = createContext(null);
+
+export const WebSocketProvider = ({ children }) => {
+    const [message, setMessage] = useState(null);
+    
+    useEffect(() => {
+        const ws = new WebSocket('ws://localhost:8000/ws/notifications');
+        
+        ws.onmessage = (event) => {
+            console.log("event", event)
+            setMessage(event.data);
+        };
+        
+        return () => {
+            ws.close();
+        };
+    }, []);
+    
+    return (
+        <WebSocketContext.Provider value={message}>
+            {children}
+        </WebSocketContext.Provider>
+    );
+};

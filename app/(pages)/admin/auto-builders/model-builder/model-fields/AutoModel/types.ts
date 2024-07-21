c7f376ce5a2b8bd0cace44717ac81e319cfb7c4c
Felix Biwott <felixkpt@gmail.com>
2024-07-21 15:48:26 +0300

    Autobuilder: edit model_fields model and related files

---
+++ app/(pages)/admin/auto-builders/model-builder/model-fields/AutoModel/types.ts
@@ -0,0 +1,22 @@
+
+type Model_fieldType = {
+id: any;
+  created_at: any;
+  updated_at: any;
+  model_builder_id: any;
+  name: any;
+  type: any;
+  label: any;
+  dataType: any;
+  defaultValue: any;
+  dropdownSource: any;
+  isVisibleInList: any;
+  isVisibleInSingleView: any;
+  isRequired: any;
+  isUnique: any;
+  dropdownDependsOn: any;
+  desktopWidth: string;
+  mobileWidth: string;
+}
+
+export default Model_fieldType;

004ffff17d7068954ede3eedc7452a0998b12c5b
Felix Biwott <felixkpt@gmail.com>
2024-07-21 15:35:14 +0300

    Autobuilder: edit model_headers model and related files

---
+++ app/(pages)/admin/auto-builders/model-builder/model-headers/AutoModel/getConstants.ts
@@ -0,0 +1,141 @@
+
+import createModelConstants from "@/app/components/baseComponents/Autos/BaseAutoModel/constants";
+import { ActionLabelsActionsType } from "@/app/components/baseComponents/Autos/BaseAutoModel/types";
+
+// Define variables for model name and API endpoint
+const modelNameSingular = 'model_headers';
+const modelURI = 'admin/auto-builders/model-builder/model-headers';
+const apiEndpoint = 'admin/auto-builders/model-builder/model-headers/';
+
+// Define fillable fields for the model_headers model
+const fillableFields = [
+  {
+    name: "model_builder_id",
+    label: "",
+    type: "dropdown",
+    dataType: "integer",
+    defaultValue: null,
+    isRequired: true,
+    isVisibleInList: true,
+    isVisibleInSingleView: true,
+    isUnique: false,
+    dropdownSource: "admin/auto-builders/model-builder",
+    dropdownDependsOn: null
+  },
+  {
+    name: "key",
+    label: "",
+    type: "input",
+    dataType: "string",
+    defaultValue: null,
+    isRequired: true,
+    isVisibleInList: true,
+    isVisibleInSingleView: true,
+    isUnique: false,
+    dropdownSource: null,
+    dropdownDependsOn: null
+  },
+  {
+    name: "label",
+    label: "",
+    type: "input",
+    dataType: "string",
+    defaultValue: null,
+    isRequired: true,
+    isVisibleInList: true,
+    isVisibleInSingleView: true,
+    isUnique: false,
+    dropdownSource: null,
+    dropdownDependsOn: null
+  },
+  {
+    name: "isVisibleInList",
+    label: "",
+    type: "input",
+    dataType: "boolean",
+    defaultValue: null,
+    isRequired: true,
+    isVisibleInList: true,
+    isVisibleInSingleView: true,
+    isUnique: false,
+    dropdownSource: null,
+    dropdownDependsOn: null
+  },
+  {
+    name: "isVisibleInSingleView",
+    label: "",
+    type: "input",
+    dataType: "string",
+    defaultValue: null,
+    isRequired: true,
+    isVisibleInList: true,
+    isVisibleInSingleView: true,
+    isUnique: false,
+    dropdownSource: null,
+    dropdownDependsOn: null
+  }
+]
+
+// Define headers for displaying in the UI (e.g., table headers, singleViewOnly means can only be shown when viewing one item not list/table)
+const headers = [
+  {
+    key: "model_builder_id",
+    label: "",
+    isVisibleInList: true,
+    isVisibleInSingleView: true
+  },
+  {
+    key: "key",
+    label: "",
+    isVisibleInList: true,
+    isVisibleInSingleView: true
+  },
+  {
+    key: "label",
+    label: "",
+    isVisibleInList: true,
+    isVisibleInSingleView: true
+  },
+  {
+    key: "isVisibleInList",
+    label: "",
+    isVisibleInList: true,
+    isVisibleInSingleView: true
+  },
+  {
+    key: "isVisibleInSingleView",
+    label: "",
+    isVisibleInList: true,
+    isVisibleInSingleView: true
+  },
+  {
+    key: "action",
+    label: "Action",
+    isVisibleInList: true,
+    isVisibleInSingleView: true
+  }
+]
+
+// Define default values for creating a new record
+const newRecordDefaults = {}
+
+// Define action labels and their associated actions
+const actionLabelsActions: ActionLabelsActionsType = {
+  viewRecord: {
+    actionType: "navigation",
+    label: ""
+  }
+};
+
+// Create model_headers constants using the createModelConstants function
+const getConstants = createModelConstants(
+  modelNameSingular,          // modelNameSingular: Name of the model/page
+  modelURI,           // modelURI: API endpoint for mode/page
+  apiEndpoint,        // apiEndpoint: API endpoint for mode/page
+  fillableFields,     // fillableFields: Fields that can be filled when creating or updating records
+  headers,            // headers: Headers to display in the UI
+  newRecordDefaults,  // newRecordDefaults: Default values for creating a new record
+  actionLabelsActions // actionLabelsActions: Actions associated with action labels
+);
+
+export default getConstants;

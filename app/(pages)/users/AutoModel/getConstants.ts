5ecae2786a15359d0669532301c4904d81791fdc
Felix Biwott <felixkpt@gmail.com>
2024-07-23 19:58:07 +0300

    Autobuilders > model-builder: edit users model and related files

---
+++ app/(pages)/users/AutoModel/getConstants.ts
@@ -0,0 +1,176 @@
+
+import createModelConstants from "@/app/components/baseComponents/Autos/BaseAutoModel/constants";
+import { ActionLabelsActionsType, FillableType, HeaderType } from "@/app/components/baseComponents/Autos/BaseAutoModel/types";
+
+// Define variables for model name and API endpoint
+const modelID = '7a4de985-542f-458e-9530-34e0d08aaa59';
+const modelNameSingular = 'user';
+const modelNamePlural = 'users';
+const modelURI = 'users';
+const apiEndpoint = 'users/';
+
+// Define fillable fields for the user model
+const fillableFields: Array<FillableType> = [
+  {
+    name: "first_name",
+    label: "",
+    type: "input",
+    dataType: "string",
+    defaultValue: null,
+    isRequired: true,
+    isVisibleInList: true,
+    isVisibleInSingleView: true,
+    isUnique: false,
+    dropdownSource: null,
+    dropdownDependsOn: null,
+    desktopWidth: 6,
+    mobileWidth: 12
+  },
+  {
+    name: "last_name",
+    label: "",
+    type: "input",
+    dataType: "string",
+    defaultValue: null,
+    isRequired: true,
+    isVisibleInList: true,
+    isVisibleInSingleView: true,
+    isUnique: false,
+    dropdownSource: null,
+    dropdownDependsOn: null,
+    desktopWidth: 6,
+    mobileWidth: 12
+  },
+  {
+    name: "email",
+    label: "",
+    type: "input",
+    dataType: "string",
+    defaultValue: null,
+    isRequired: true,
+    isVisibleInList: true,
+    isVisibleInSingleView: true,
+    isUnique: true,
+    dropdownSource: null,
+    dropdownDependsOn: null,
+    desktopWidth: 12,
+    mobileWidth: 12
+  },
+  {
+    name: "phone_number",
+    label: "",
+    type: "input",
+    dataType: "string",
+    defaultValue: null,
+    isRequired: false,
+    isVisibleInList: true,
+    isVisibleInSingleView: true,
+    isUnique: false,
+    dropdownSource: null,
+    dropdownDependsOn: null,
+    desktopWidth: 12,
+    mobileWidth: 12
+  },
+  {
+    name: "password",
+    label: "",
+    type: "input",
+    dataType: "password",
+    defaultValue: null,
+    isRequired: true,
+    isVisibleInList: false,
+    isVisibleInSingleView: false,
+    isUnique: false,
+    dropdownSource: null,
+    dropdownDependsOn: null,
+    desktopWidth: 12,
+    mobileWidth: 12
+  },
+  {
+    name: "password_confirmation",
+    label: "",
+    type: "input",
+    dataType: "password",
+    defaultValue: null,
+    isRequired: true,
+    isVisibleInList: false,
+    isVisibleInSingleView: false,
+    isUnique: false,
+    dropdownSource: null,
+    dropdownDependsOn: null,
+    desktopWidth: 12,
+    mobileWidth: 12
+  }
+]
+
+// Define headers for displaying in the UI (e.g., table headers, singleViewOnly means can only be shown when viewing one item not list/table)
+const headers: HeaderType[] = [
+  {
+    key: "first_name",
+    label: "",
+    isVisibleInList: true,
+    isVisibleInSingleView: true
+  },
+  {
+    key: "last_name",
+    label: "",
+    isVisibleInList: true,
+    isVisibleInSingleView: true
+  },
+  {
+    key: "email",
+    label: "",
+    isVisibleInList: true,
+    isVisibleInSingleView: true
+  },
+  {
+    key: "phone_number",
+    label: "",
+    isVisibleInList: true,
+    isVisibleInSingleView: true
+  },
+  {
+    key: "action",
+    label: "Action",
+    isVisibleInList: true,
+    isVisibleInSingleView: true
+  }
+]
+
+// Define default values for creating a new record
+const newRecordDefaults = {}
+
+// Define action labels and their associated actions
+const actionLabelsActions: ActionLabelsActionsType = {
+  viewRecord: {
+    actionType: "navigation",
+    label: ""
+  },
+  editRecord: {
+    actionType: "modal",
+    label: ""
+  },
+  updateRecordStatus: {
+    actionType: "modal",
+    label: ""
+  },
+  deleteRecord: {
+    actionType: "modal",
+    label: ""
+  }
+};
+
+// Create user constants using the createModelConstants function
+const getConstants = createModelConstants(
+  modelID,          // modelID: UNIQUE ID of the model/page
+  modelNameSingular,          // modelNameSingular: Name of the model/page
+  modelNamePlural,          // modelNamePlural: Plural name of the model/page
+  modelURI,           // modelURI: API endpoint for mode/page
+  apiEndpoint,        // apiEndpoint: API endpoint for mode/page
+  fillableFields,     // fillableFields: Fields that can be filled when creating or updating records
+  headers,            // headers: Headers to display in the UI
+  newRecordDefaults,  // newRecordDefaults: Default values for creating a new record
+  actionLabelsActions // actionLabelsActions: Actions associated with action labels
+);
+
+export default getConstants;
---
+++ app/(pages)/users/AutoModel/types.ts
@@ -0,0 +1,14 @@
+
+type UserType = {
+id: any;
+  created_at: any;
+  updated_at: any;
+  first_name: any;
+  last_name: any;
+  email: any;
+  phone_number: any;
+  password: any;
+  password_confirmation: any;
+}
+
+export default UserType;

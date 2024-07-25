8b020d782003063729e3fc8e6ea11187f975d310
Felix Biwott <felixkpt@gmail.com>
2024-07-26 01:10:55 +0300

    basic functionality

---
+++ app/(pages)/auto-builders/model-builder/[model-builder]/edit/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Builder from '../../Module/components/Builder';
+import { saveAndGenerateModel } from '../../Module/utils/modelGenerator';
+import { inputTypes } from '../../Module/utils/constants';
+import getDropdownSourcesList from '../../Module/utils/getDropdownSourcesList';
+import { AutoPageBuilderType } from '../../Module/utils/backendTypes';
+import getConstants from '../../AutoModel/getConstants';
+
+const Page = async () => {
+
+  // Destructure constants from getConstants
+  const {
+    MODEL_ID,
+    MODEL_NAME_PLURAL,
+    API_ENDPOINT,
+  } = getConstants;
+
+  const dropdownSourcesList: AutoPageBuilderType[] = await getDropdownSourcesList(API_ENDPOINT, MODEL_ID, MODEL_NAME_PLURAL)
+
+  return <Builder inputTypes={inputTypes} dropdownSourcesList={dropdownSourcesList} saveAndGenerateModel={saveAndGenerateModel} />;
+};
+
+export default Page;

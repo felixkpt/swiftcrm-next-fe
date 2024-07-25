3559ae99770442d1cdcf1fad6b79f064b2860830
Felix Biwott <felixkpt@gmail.com>
2024-07-25 20:51:09 +0300

    Autobuilders > model-builder: edit model-builders model and related files

--- app/(pages)/auto-builders/model-builder/AutoModel/getConstants.ts
+++ app/(pages)/auto-builders/model-builder/AutoModel/getConstants.ts
@@ -3,7 +3,7 @@ import createModelConstants from "@/app/components/baseComponents/Autos/BaseAuto
 import { ActionLabelsActionsType, FillableType, HeaderType } from "@/app/components/baseComponents/Autos/BaseAutoModel/types";
 
 // Define variables for model name and API endpoint
-const modelID = '50490790-9eac-49a2-841e-f49376bb48aa';
+const modelID = '7eb9b5d5-9b61-4b4b-8b77-39ef96634845';
 const modelNameSingular = 'model-builder';
 const modelNamePlural = 'model-builders';
 const modelURI = 'auto-builders/model-builder';
